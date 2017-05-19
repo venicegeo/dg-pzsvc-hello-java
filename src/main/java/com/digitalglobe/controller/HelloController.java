62c983139fab9292021ff03f31fa8111a2395e26
Marge Lynum <mmlynum@gmail.com>
2017-05-19 06:39:32 -0400

    Slight updates

--- src/main/java/com/digitalglobe/controller/HelloController.java
+++ src/main/java/com/digitalglobe/controller/HelloController.java
@@ -26,7 +26,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 /**
